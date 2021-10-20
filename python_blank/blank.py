d9d61d7c9082bfe9558637b4034a9adfd1560d1a
Tim Fechner <tim.fechner@mailbox.org>
2021-10-20 19:49:30 +0200

    Add python blank

---
+++ python_blank/blank.py
@@ -0,0 +1,27 @@
+"""Empty the canvas once"""
+
+import os
+
+from telnetlib import Telnet
+
+
+HOST = os.getenv('PIXELFLUT_HOST')
+PORT = os.getenv('PIXELFLUT_PORT')
+
+HEIGHT = 1080
+WIDTH = 1920
+
+COLOR = '000000'
+
+
+def draw_blank(tn):
+    line = str()
+    for x in range(0, WIDTH):
+        for y in range(0, HEIGHT):
+            line += f'PX {x} {y} {COLOR}\n'
+    tn.write(line.encode())
+
+
+if __name__ == '__main__':
+    with Telnet(HOST, PORT) as tn:
+        draw_blank(tn)
