00946589d8bd93b392a1651fba578ad97bc5ca2d
borgolte <g.borgolte96@gmail.com>
2021-10-20 20:02:18 +0200

    fix gitIt

--- python_gif/python_gif.py
+++ python_gif/python_gif.py
@@ -1,5 +1,7 @@
 from PIL import Image
-from python_connector.connector import rgb_to_hex, Connector
+from python_utils.colors import rgb_dec_to_hex
+from os.path import exists
+import pickle
 
 
 class Gif:
@@ -22,17 +24,46 @@ class Gif:
             print("Frame number: {}".format(frame_number))
             yield image_object
 
-    def frame_to_pixel(self, frame):
+    def _frame_to_pixel(self, frame):
         y, x = frame.size
         row = 0
         pixel_data = []
         while row <= y - 1:
             column = 0
             while column <= x - 1:
-                rgb = frame.convert('RGB').getpixel((row, column))
-                hex_code = rgb_to_hex(rgb)
+                r, g, b = frame.convert('RGB').getpixel((row, column))
+                hex_code = rgb_dec_to_hex(r, g, b)
                 pixel_data.append((row, column, hex_code))
                 #data = data + "PX {x} {y} {hex}\n".format(x=row, y=column, hex=hex_code)
                 column = column + 1
             row = row + 1
         return pixel_data
+
+    def get_frames_as_pixel_set(self, start_x=None, start_y=None):
+        gif_name = self.file_path.split('.')[0]
+        pickle_file = gif_name + '.pkl'
+        if not exists(pickle_file):
+            frames = []
+            for frame in self.get_frames():
+                frames.append(self._frame_to_pixel(frame))
+            with open(gif_name + '.pkl', 'wb') as f:
+                pickle.dump(frames, f)
+        else:
+            with open(pickle_file, 'rb') as f:
+                frames = pickle.load(f)
+
+        if start_x and start_y:
+            offset_frames = []
+            for frame in frames:
+                offset_frames.append(self.set_offset(start_x, start_y, frame))
+
+            frames = offset_frames
+
+        return frames
+
+    def set_offset(self, start_x, start_y, frame):
+        offset_frame = []
+        for x, y, color in frame:
+            offset_frame.append((x + start_x, y + start_y, color))
+        return offset_frame
+
